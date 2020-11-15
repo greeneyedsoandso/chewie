50a4e61104930cd44f6f134fe966b570e1672ccd
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-15 15:41:27 -0800

    test bot with example from discord.py

---
+++ example_bot.py
@@ -0,0 +1,21 @@
+# hello world discord bot
+"""Issues a greeting when someone logs on"""
+import discord
+from config import token
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
